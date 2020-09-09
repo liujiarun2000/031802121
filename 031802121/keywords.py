791247952fb62e3cfe7e05a69729839f044a77e8
liujiarun2000 <1395844081@qq.com>
2020-09-09 23:20:52 +0800

    extract keywords

---
+++ 031802121/keywords.py
@@ -0,0 +1,20 @@
+import re
+import jieba
+import jieba.analyse
+import html
+from datasketch import MinHash
+
+
+class minhashSimilarity(object):
+
+    def __init__(self, content_x1, content_y2):
+        self.s1 = content_x1
+        self.s2 = content_y2
+
+    @staticmethod
+    def extract_keyword(content):  # 提取关键词
+        # 切割
+        seg = [i for i in jieba.cut(content, cut_all=True) if i != '']
+        # 提取关键词
+        keywords = jieba.analyse.extract_tags("|".join(seg), topK=200, withWeight=False)
+        return keywords
